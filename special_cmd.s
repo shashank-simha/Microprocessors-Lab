8d38154a821a8a43c3296b9f922d6c10766cb0ba
shashank-simha <shashanksimha183@gmail.com>
2018-09-29 08:38:50 +0530

    special operations

---
+++ special_cmd.s
@@ -0,0 +1,47 @@
+.include "general.mac"
+
+.intel_mnemonic
+.intel_syntax noprefix
+
+.data
+	res: .ds 4
+	
+.text
+	.global _start
+
+_start:
+	//daa
+	mov eax, 9
+	mov ebx, 9
+	add eax, ebx
+	daa
+	
+	//aaa
+	mov eax, '9'
+	mov ebx, '9'
+	add eax, ebx
+	aaa
+	
+	//aad
+	mov ax, 0x0205
+	aad
+	
+	//aam
+	mov eax, 9
+	mov ebx, 9
+	mul ebx
+	aam
+	
+	//das
+	mov eax, 9
+	mov ebx, 4
+	sub eax, ebx
+	daa
+	
+	//aas
+	mov eax, '9'
+	mov ebx, '4'
+	sub eax, ebx
+	aaa
+	
+exit
